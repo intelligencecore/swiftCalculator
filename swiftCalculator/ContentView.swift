ca6a04961bf6cb2e96991315223f4212e0f5070a
Intelligencecore <07wood.temblor@icloud.com>
2026-02-09 20:49:48 -0800

    Initial Commit

---
+++ swiftCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftCalculator
+//
+//  Created by Richier on 2/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftCalculator/swiftCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftCalculatorApp.swift
+//  swiftCalculator
+//
+//  Created by Richier on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
