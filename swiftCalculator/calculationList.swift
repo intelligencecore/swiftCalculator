4aff51a2a9ba9a35189e77cf4a49e953d34660d7
Intelligencecore <07wood.temblor@icloud.com>
2026-02-15 01:01:15 -0800

    Small Upgrades

---
+++ swiftCalculator/calculationList.swift
@@ -0,0 +1,18 @@
+//
+//  calculationList.swift
+//  swiftCalculator
+//
+//  Created by Richier on 2/14/26.
+//
+
+import SwiftUI
+
+struct calculationList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    calculationList()
+}
