346b85f37e63823f422bd4d2fff5106f245da7a8
Intelligencecore <07wood.temblor@icloud.com>
2026-03-23 22:39:19 -0700

    Minimal changes

--- swiftCalculator/moneyExchange.swift
+++ swiftCalculator/moneyExchange.swift
@@ -13,33 +13,25 @@ import SwiftUI
 struct MoneyExchange: View {
 	
 	var body: some View {
-		
-		Text("There will be a menu here to do money exchange from one currency to another")
-			.font(Font.largeTitle)
-		Spacer()
 		VStack{
 			
 			Menu("Select Currency"){
 				Button("American Dollar"){
-					
+						// calculation
 				}
 				Button("Canadian Dollar"){
-					
+						// calculation
 				}
 				Button("Euro"){
-					
+						// calculation
 				}
 				
 			}
-			
-			
-			
-			
-			
 		}
-		Spacer()
+		.background(Color.blue)
+		}
 	}
-}
+
 
 #Preview {
 	MoneyExchange()
